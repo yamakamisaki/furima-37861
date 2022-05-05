15e3971a36fbe4e06504c1981622efaf54345af0
yamakamisaki <s.chuuu02@gmail.com>
2022-05-05 17:34:10 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
-    validates :password ,format: {with: (/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i)}
-     # ひらがな、カタカナ、漢字のみ許可する
-    validates :first_name , format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-    validates :last_name ,  format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    # 英字、数字が含まれている場合のみ許可する
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    # ひらがな、カタカナ、漢字のみ許可する
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :last_name,  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     # カタカナのみ許可する
-    validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/}
-    validates :last_name_kana , format: {with: /\A[ァ-ヶー]+\z/}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
   end
 end
