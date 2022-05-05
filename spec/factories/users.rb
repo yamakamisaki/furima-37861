9f0a049cec1416300c26554854194ab0da5b3ea9
yamakamisaki <s.chuuu02@gmail.com>
2022-05-05 17:24:43 +0900

    userモデル単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 5)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name            {'山田'}
+    last_name             {'一郎'}
+    first_name_kana       {'ヤマダ'}
+    last_name_kana        {'イチロウ'}
+    birthday              { '2003/07/26' }
+
+  end
+end
