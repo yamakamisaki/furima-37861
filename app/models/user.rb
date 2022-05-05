dab520d8b4c29f5eb79d604e7324509671a8b300
yamakamisaki <s.chuuu02@gmail.com>
2022-05-05 14:49:17 +0900

    devise導入・マイグレーションファイル記述

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+     # ひらがな、カタカナ、漢字のみ許可する
+    validates :first_name , format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name ,  format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    # カタカナのみ許可する
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :last_name_kana , format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+  end
+end
