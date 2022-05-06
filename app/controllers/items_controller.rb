bd00d6ac0f8e6537dbf37063ce143e6287984524
yamakamisaki <s.chuuu02@gmail.com>
2022-05-06 20:41:20 +0900

    出品機能のデータを保存する処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,26 @@
 class ItemsController < ApplicationController
+
+def index
+  # @item = Item.new(item_params)
+end
+
+def new
+  @item = Item.new
+end
+
+def create
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:title,:text,:image,:category_id,:condition_id,:shipping_fee_id,:prefecture_id,:send_day_id,:price).merge(user_id: current_user.id)
+end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,19 +3,21 @@ class Item < ApplicationRecord
   has_one_attached :image
   
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to [:category, :condition, :shipping_fee, :prefecture, :send_day]
+  belongs_to :category, :condition, :shippingFee, :prefecture, :sendDay
 
   with_options presence: true do
     validates :title
-    validates :text
+    validates :image
+    # 1~1000文字の範囲のみ保存可能
+    validates :text   ,length: { in: 1..1000 }
     # 半角数字・¥300~¥9,999,999の間のみ保存可能
-    validates :price, format: { with: /\d{300,9999999}/ }
-
-    # ActiveHashのid:1は保存できない  { id: 1, name: '--' }
-    validates :category_id, numericality: { other_than: 1 } 
-    validates :condition_id, numericality: { other_than: 1 } 
-    validates :shipping_fee_id, numericality: { other_than: 1 } 
-    validates :prefecture_id, numericality: { other_than: 1 } 
-    validates :send_day_id, numericality: { other_than: 1 } 
+    validates :price  , numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+    format: { with: /\A[0-9]+\z/ }
   end
+     # ActiveHashのid:1は保存できない  { id: 1, name: '--' }
+     validates :category_id, numericality: { other_than: 1 } 
+     validates :condition_id, numericality: { other_than: 1 } 
+     validates :shipping_fee_id, numericality: { other_than: 1 } 
+     validates :prefecture_id, numericality: { other_than: 1 } 
+     validates :send_day_id, numericality: { other_than: 1 } 
 end
