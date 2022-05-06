9d9f5b841d07594f629babe465da5238ecdca153
yamakamisaki <s.chuuu02@gmail.com>
2022-05-06 11:46:46 +0900

    itemモデルにバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,17 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to [:category, :condition, :shipping_fee, :prefecture, :send_day]
 
+  with_options presence: true do
+    validates :title
+    validates :text
+    # 半角数字・¥300~¥9,999,999の間のみ保存可能
+    validates :price, format: { with: /\d{300,9999999}/ }
 
+    # ActiveHashのid:1は保存できない  { id: 1, name: '--' }
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :condition_id, numericality: { other_than: 1 } 
+    validates :shipping_fee_id, numericality: { other_than: 1 } 
+    validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :send_day_id, numericality: { other_than: 1 } 
+  end
 end
