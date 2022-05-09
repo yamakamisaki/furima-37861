49effed92ca6b0de6c4d717926473e68c2739e06
yamakamisaki <s.chuuu02@gmail.com>
2022-05-09 16:03:54 +0900

    unless → if に記述を変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy unless current_user.id == @item.user_id
+    @item.destroy if current_user.id == @item.user_id
     redirect_to root_path
   end
 
