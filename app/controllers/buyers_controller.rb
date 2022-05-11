2843b865336d0d659dff06250adb18658eb97de7
yamakamisaki <s.chuuu02@gmail.com>
2022-05-11 17:47:06 +0900

    不要な記述削除・繰り返しの記述のまとめ修正

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,14 +1,13 @@
 class BuyersController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :set_item_id, only: [:index, :create]
 
   def index
     @buyer_address = BuyerAddress.new
-    @item = Item.find(params[:item_id])
     redirect_to root_path if @item.buyer.present? || current_user.id == @item.user_id
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buyer_address = BuyerAddress.new(buyer_params)
     if @buyer_address.valid?
       pay_item
@@ -20,6 +19,10 @@ class BuyersController < ApplicationController
   end
 
   private
+
+  def set_item_id
+    @item = Item.find(params[:item_id])
+  end
 
   def buyer_params
     params.require(:buyer_address).permit(:postal_code, :prefecture_id,
